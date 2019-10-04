44f3a514ea9bbe8ca8d14fd13fd7cd646eb46ff8
Ben Plummer <ben.james.plummer@gmail.com>
2019-10-04 13:30:31 +0100

    Replace tabs with spaces

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         PersonFetcher pf = new PersonFetcher();
         pf.run();
     }
